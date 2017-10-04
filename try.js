908ca5a3683e2dc4f9ae8b494dc914c1f43a560b
DESKTOP-DJ3ROVM\Pshin <kyneeva@gmail.com>
2017-10-04 23:25:26 +0300

    my first functions

---
+++ try.js
@@ -0,0 +1,36 @@
+var chihuahua = {};
+var toyterrier = {};
+
+chihuahua = {
+    dog: 'Elya',
+    game: function () {
+        console.log('gav gav')
+    },
+    run: function () {
+        console.log('бег')
+    },
+	mydog: function () {
+        this.dog ='beloved';
+        console.log(this.dog);
+    }
+}
+chihuahua.mydog();
+chihuahua.game();
+chihuahua.run();
+
+toyterrier = {
+    dog: 'Gabby',
+    sleep:  function () {
+        console.log('Рррр')
+    },
+    eat: function () {
+        console.log('ням ням')
+    },
+    mydog: function () {
+        this.dog ='beloved';
+        console.log(this.dog);
+    }
+}
+toyterrier.mydog();
+toyterrier.sleep();
+toyterrier.eat();
